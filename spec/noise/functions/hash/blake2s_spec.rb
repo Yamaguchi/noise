106ca46ce8d0ed176d2a29af1f846ee41e52dbcf
Hajime Yamaguchi <h_yamaguchi@chaintope.com>
2019-04-19 12:44:48 +0900

    Add Test Vectors from https://github.com/BLAKE2/BLAKE2/blob/master/testvectors/blake2s-kat.txt

--- spec/noise/functions/hash/blake2s_spec.rb
+++ spec/noise/functions/hash/blake2s_spec.rb
@@ -20,14 +20,6 @@ RSpec.describe Noise::Functions::Hash::Blake2s do
       it { is_expected.to eq response }
     end
 
-    # https://github.com/BLAKE2/BLAKE2/blob/master/testvectors/blake2s-kat.txt
-    describe '00' do
-      let(:data) { "00".htb }
-      let(:key) { "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f".htb }
-      let(:response) { "40d15fee7c328830166ac3f918650f807e7e01e177258cdc0a39b11f598066f1" }
-      it { is_expected.to eq response }
-    end
-
     describe 'empty' do
       let(:data) { "".htb }
       let(:key) { "".htb }
@@ -35,20 +27,19 @@ RSpec.describe Noise::Functions::Hash::Blake2s do
       it { is_expected.to eq response }
     end
 
-    describe 'long data' do
-      let(:data) do
-        '000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f' \
-        '202122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f' \
-        '404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f' \
-        '606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e7f' \
-        '808182838485868788898a8b8c8d8e8f909192939495969798999a9b9c9d9e9f' \
-        'a0a1a2a3a4a5a6a7a8a9aaabacadaeafb0b1b2b3b4b5b6b7b8b9babbbcbdbebf' \
-        'c0c1c2c3c4c5c6c7c8c9cacbcccdcecfd0d1d2d3d4d5d6d7d8d9dadbdcdddedf' \
-        'e0e1e2e3e4e5e6e7e8e9eaebecedeeeff0f1f2f3f4f5f6f7f8f9fafbfcfdfe'.htb
+    # https://github.com/BLAKE2/BLAKE2/blob/master/testvectors/blake2s-kat.txt
+    describe 'test vector' do
+      require "json"
+      path = "spec/vectors/blake2s-kat.txt"
+      vectors = JSON.parse(File.read(path), symbolize_names: true)
+      vectors.each do |json|
+        describe "#{json[:in]}" do
+          let(:data) { json[:in].htb }
+          let(:key) { json[:key].htb }
+          let(:response) { json[:hash] }
+          it { is_expected.to eq response }
+        end
       end
-      let(:key) { "000102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f".htb }
-      let(:response) { "3fb735061abc519dfe979e54c1ee5bfad0a9d858b3315bad34bde999efd724dd" }
-      it { is_expected.to eq response }
     end
   end
 end
