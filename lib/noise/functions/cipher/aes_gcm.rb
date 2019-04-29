ba6fa5f0b55278ae9e9cacf21a28ca8c572627b0
Hajime Yamaguchi <gen.yamaguchi0@gmail.com>
2019-04-29 15:03:59 +0900

    Implement #rekey(k) methods for cipher class

--- lib/noise/functions/cipher/aes_gcm.rb
+++ lib/noise/functions/cipher/aes_gcm.rb
@@ -4,6 +4,8 @@ module Noise
   module Functions
     module Cipher
       class AesGcm
+        MAX_NONCE = 2**64 - 1
+
         def encrypt(k, n, ad, plaintext)
           cipher = OpenSSL::Cipher::AES.new(256, :GCM).encrypt
           cipher.key = k
@@ -23,6 +25,16 @@ module Noise
 
         def nonce_to_bytes(n)
           "\x00" * 4 + format('%16x', n).htb
+        end
+
+        # Returns a new 32-byte cipher key as a pseudorandom function of k.
+        # If this function is not specifically defined for some set of cipher
+        # functions, then it defaults to returning the first 32 bytes from
+        # ENCRYPT(k,maxnonce, zerolen, zeros), where  maxnonce equals 264-1,
+        # zerolen is a zero-length byte sequence, and zeros is a sequence of
+        # 32 bytes filled with zeros.
+        def rekey(k)
+          encrypt(k, MAX_NONCE, '', "\x00" * 32)[0...32]
         end
       end
     end
--- lib/noise/functions/cipher/cha_cha_poly.rb
+++ lib/noise/functions/cipher/cha_cha_poly.rb
@@ -4,22 +4,34 @@ module Noise
   module Functions
     module Cipher
       class ChaChaPoly
+        MAX_NONCE = 2**64 - 1
+
         def encrypt(k, n, ad, plaintext)
-          @cipher = RbNaCl::AEAD::ChaCha20Poly1305IETF.new(String.new(k).force_encoding('ASCII-8BIT'))
-          @cipher.encrypt(nonce_to_bytes(n), plaintext, ad)
+          cipher = RbNaCl::AEAD::ChaCha20Poly1305IETF.new(String.new(k).force_encoding('ASCII-8BIT'))
+          cipher.encrypt(nonce_to_bytes(n), plaintext, ad)
         rescue ::RbNaCl::CryptoError => e
           raise Noise::Exceptions::EncryptError, e
         end
 
         def decrypt(k, n, ad, ciphertext)
-          @cipher = RbNaCl::AEAD::ChaCha20Poly1305IETF.new(String.new(k).force_encoding('ASCII-8BIT'))
-          @cipher.decrypt(nonce_to_bytes(n), ciphertext, ad)
+          cipher = RbNaCl::AEAD::ChaCha20Poly1305IETF.new(String.new(k).force_encoding('ASCII-8BIT'))
+          cipher.decrypt(nonce_to_bytes(n), ciphertext, ad)
         rescue ::RbNaCl::CryptoError => e
           raise Noise::Exceptions::DecryptError, e
         end
 
         def nonce_to_bytes(n)
           "\x00" * 4 + format('%16x', n).htb.reverse
+        end
+
+        # Returns a new 32-byte cipher key as a pseudorandom function of k.
+        # If this function is not specifically defined for some set of cipher
+        # functions, then it defaults to returning the first 32 bytes from
+        # ENCRYPT(k,maxnonce, zerolen, zeros), where  maxnonce equals 2**64-1,
+        # zerolen is a zero-length byte sequence, and zeros is a sequence of
+        # 32 bytes filled with zeros.
+        def rekey(k)
+          encrypt(k, MAX_NONCE, '', "\x00" * 32)[0..32]
         end
       end
     end
